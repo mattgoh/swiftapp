9671b1a4cdff737ae73ec898e360d8ac68222445
Matt <matt@kawas.local>
2022-07-13 19:41:16 -0700

    update storyboard

--- randomphoto/randomphoto/ViewController.swift
+++ randomphoto/randomphoto/ViewController.swift
@@ -16,6 +16,14 @@ class ViewController: UIViewController {
         return imageView
     }()
     
+    private let button: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .white
+        button.setTitle("Random Photo", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        return button
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemPurple
@@ -23,8 +31,23 @@ class ViewController: UIViewController {
         imageView.frame = CGRect(x: 0, y: 0, width: 300, height: 300)
         imageView.center = view.center
         
+        view.addSubview(button)
         getRandomPhoto()
+        button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
 
+    }
+
+    @objc func didTapButton(){
+        getRandomPhoto()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        button.frame = CGRect(x: 30,
+                              y: view.frame.size.height-150-view.safeAreaInsets.bottom,
+                              width: view.frame.size.width-60,
+                              height: 60)
     }
 
     func getRandomPhoto() {
