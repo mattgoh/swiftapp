d915969f2e539ec38c65a1bfd41a8ab1e86c4fee
Matt <matt@kawas.local>
2022-07-13 19:14:22 -0700

    first commit

---
+++ randomphoto/randomphoto/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  randomphoto
+//
+//  Created by Matt on 7/13/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.backgroundColor = .white
+        return imageView
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPurple
+        view.addSubview(imageView)
+        imageView.frame = CGRect(x: 0, y: 0, width: 300, height: 300)
+        imageView.center = view.center
+        
+        getRandomPhoto()
+
+    }
+
+    func getRandomPhoto() {
+        let urlString =
+         "https://source.unsplash.com/random/600x600"
+        let url = URL(string: urlString)!
+        guard let data = try? Data(contentsOf: url) else {
+            return
+        }
+        imageView.image = UIImage(data: data)
+    }
+    
+}
+
